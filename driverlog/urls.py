e30b365c5bb2d6e21cfc8ea9367dcbf56db328b7
Codinplus31 <codinplus31@gmail.com>
2026-01-22 13:06:31 +0100

    initialized project and finished assessment

---
+++ driverlog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import generate_trip, health_check
+
+urlpatterns = [
+    path('api/trip/', generate_trip, name='generate_trip'),
+    path('health/', health_check, name='health_check'),
+]
